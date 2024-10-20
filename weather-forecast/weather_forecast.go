172d546996d1d03e6481aaeca3f8b37338ede478
Exiles.prx <exiles.prx@gmail.com>
2024-10-20 19:18:17 -0400

    Solved exercise weather forecast

--- weather-forecast/weather_forecast.go
+++ weather-forecast/weather_forecast.go
@@ -1,8 +1,13 @@
+// Package weather provides a library for obtaining weather forecast information.
 package weather
 
+// CurrentCondition is a global variable to store the current weather condition.
 var CurrentCondition string
+
+// CurrentLocation is a global variable to store the current location.
 var CurrentLocation string
 
+// Forecast returns the current weather condition for the given city.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
