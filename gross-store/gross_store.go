7bdbb0bd6065fcbc5bc457fa35889111bfdb7901
Exiles.prx <exiles.prx@gmail.com>
2024-11-08 18:07:59 -0500

    Solved exercise gross store

--- gross-store/gross_store.go
+++ gross-store/gross_store.go
@@ -2,25 +2,60 @@ package gross
 
 // Units stores the Gross Store unit measurements.
 func Units() map[string]int {
-	panic("Please implement the Units() function")
+	return map[string]int{
+		"quarter_of_a_dozen": 3,
+		"half_of_a_dozen":    6,
+		"dozen":              12,
+		"small_gross":        120,
+		"gross":              144,
+		"great_gross":        1728,
+	}
 }
 
 // NewBill creates a new bill.
 func NewBill() map[string]int {
-	panic("Please implement the NewBill() function")
+	return map[string]int{}
 }
 
 // AddItem adds an item to customer bill.
 func AddItem(bill, units map[string]int, item, unit string) bool {
-	panic("Please implement the AddItem() function")
+	_, exists := units[unit]
+	if !exists {
+		return false
+	}
+
+	bill[item] += units[unit]
+	return true
 }
 
 // RemoveItem removes an item from customer bill.
 func RemoveItem(bill, units map[string]int, item, unit string) bool {
-	panic("Please implement the RemoveItem() function")
+	_, existsInBill := bill[item]
+	_, existsInUnits := units[unit]
+	if !existsInBill || !existsInUnits {
+		return false
+	}
+
+	quantity := bill[item] - units[unit]
+	if quantity < 0 {
+		return false
+	}
+
+	if quantity == 0 {
+		delete(bill, item)
+		return true
+	}
+
+	bill[item] = quantity
+	return true
 }
 
 // GetItem returns the quantity of an item that the customer has in his/her bill.
 func GetItem(bill map[string]int, item string) (int, bool) {
-	panic("Please implement the GetItem() function")
+  value, exists := bill[item]
+  if !exists {
+    return 0, false
+  }
+
+  return value, true
 }
