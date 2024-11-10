cc46e3ee1da30135f3560cdf64f0ea067fa3ec28
Exiles.prx <exiles.prx@gmail.com>
2024-11-09 23:31:17 -0500

    Solved exercise sorting room

--- sorting-room/sorting_room.go
+++ sorting-room/sorting_room.go
@@ -1,8 +1,13 @@
 package sorting
 
+import (
+	"fmt"
+	"strconv"
+)
+
 // DescribeNumber should return a string describing the number.
 func DescribeNumber(f float64) string {
-	panic("Please implement DescribeNumber")
+	return fmt.Sprintf("This is the number %.1f", f)
 }
 
 type NumberBox interface {
@@ -11,7 +16,7 @@ type NumberBox interface {
 
 // DescribeNumberBox should return a string describing the NumberBox.
 func DescribeNumberBox(nb NumberBox) string {
-	panic("Please implement DescribeNumberBox")
+	return fmt.Sprintf("This is a box containing the number %.1f", float32(nb.Number()))
 }
 
 type FancyNumber struct {
@@ -29,15 +34,32 @@ type FancyNumberBox interface {
 // ExtractFancyNumber should return the integer value for a FancyNumber
 // and 0 if any other FancyNumberBox is supplied.
 func ExtractFancyNumber(fnb FancyNumberBox) int {
-	panic("Please implement ExtractFancyNumber")
+	switch fnb.(type) {
+	case FancyNumber:
+		n, _ := strconv.Atoi(fnb.Value())
+		return n
+	default:
+		return 0
+	}
 }
 
 // DescribeFancyNumberBox should return a string describing the FancyNumberBox.
 func DescribeFancyNumberBox(fnb FancyNumberBox) string {
-	panic("Please implement DescribeFancyNumberBox")
+	return fmt.Sprintf("This is a fancy box containing the number %.1f", float32(ExtractFancyNumber(fnb)))
 }
 
 // DescribeAnything should return a string describing whatever it contains.
 func DescribeAnything(i interface{}) string {
-	panic("Please implement DescribeAnything")
+	switch n := i.(type) {
+	case int:
+		return DescribeNumber(float64(n))
+	case float64:
+		return DescribeNumber(n)
+	case NumberBox:
+		return DescribeNumberBox(n)
+	case FancyNumberBox:
+		return DescribeFancyNumberBox(n)
+	default:
+		return "Return to sender"
+	}
 }
