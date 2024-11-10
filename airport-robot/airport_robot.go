6de2cff6537ee867c788f71bc1c9e65ec277d5cf
Exiles.prx <exiles.prx@gmail.com>
2024-11-09 19:13:25 -0500

    Solved exercise airport robot

--- airport-robot/airport_robot.go
+++ airport-robot/airport_robot.go
@@ -1,5 +1,37 @@
 package airportrobot
 
-// Write your code here.
-// This exercise does not have tests for each individual task.
-// Try to solve all the tasks first before running the tests.
+import "fmt"
+
+type Greeter interface {
+	Language() string
+	Greet(name string) string
+}
+
+// SayHello greets the given name in the language of the greeter
+func SayHello(name string, greeter Greeter) string {
+	return fmt.Sprintf("I can speak %s: %s!", greeter.Language(), greeter.Greet(name))
+}
+
+type Italian struct{}
+
+// Language returns the language of the greeter
+func (i Italian) Language() string {
+	return "Italian"
+}
+
+// Greet greets the given name
+func (i Italian) Greet(name string) string {
+	return fmt.Sprintf("Ciao %s", name)
+}
+
+type Portuguese struct{}
+
+// Language returns the language of the greeter
+func (p Portuguese) Language() string {
+	return "Portuguese"
+}
+
+// Greet greets the given name
+func (p Portuguese) Greet(name string) string {
+	return fmt.Sprintf("Olá %s", name)
+}
