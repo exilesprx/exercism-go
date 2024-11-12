5e9cb2d9456ea5f657697d1858ddff2a49c24dbf
Exiles.prx <exiles.prx@gmail.com>
2024-11-11 22:23:03 -0500

    Solved exercise the farm

--- the-farm/the_farm.go
+++ the-farm/the_farm.go
@@ -1,16 +1,55 @@
 package thefarm
 
-// TODO: define the 'DivideFood' function
+import (
+	"errors"
+	"fmt"
+)
 
-// TODO: define the 'ValidateInputAndDivideFood' function
+func DivideFood(cal FodderCalculator, cows int) (float64, error) {
+	fodderAmount, err := cal.FodderAmount(cows)
+	if err != nil {
+		return 0, err
+	}
 
-// TODO: define the 'ValidateNumberOfCows' function
+	flatteningFactor, err := cal.FatteningFactor()
+	if err != nil {
+		return 0, err
+	}
 
-// Your first steps could be to read through the tasks, and create
-// these functions with their correct parameter lists and return types.
-// The function body only needs to contain `panic("")`.
-//
-// This will make the tests compile, but they will fail.
-// You can then implement the function logic one by one and see
-// an increasing number of tests passing as you implement more
-// functionality.
+	return fodderAmount * flatteningFactor / float64(cows), nil
+}
+
+func ValidateInputAndDivideFood(cal FodderCalculator, cows int) (float64, error) {
+	if cows > 0 {
+		return DivideFood(cal, cows)
+	}
+
+	return 0, errors.New("invalid number of cows")
+}
+
+type InvalidCowsError struct {
+	cows    int
+	message string
+}
+
+func (e *InvalidCowsError) Error() string {
+	return fmt.Sprintf("%d cows are invalid: %s", e.cows, e.message)
+}
+
+func ValidateNumberOfCows(cows int) error {
+	if cows < 0 {
+		return &InvalidCowsError{
+			message: "there are no negative cows",
+			cows:    cows,
+		}
+	}
+
+	if cows == 0 {
+		return &InvalidCowsError{
+			message: "no cows don't need food",
+			cows:    0,
+		}
+	}
+
+	return nil
+}
