2dfe67a6b61288e527ce8e21597bec17fe8cd37b
Exiles.prx <exiles.prx@gmail.com>
2024-11-17 12:08:45 -0500

    Solved exercise parsing log files

--- parsing-log-files/parsing_log_files.go
+++ parsing-log-files/parsing_log_files.go
@@ -1,21 +1,49 @@
 package parsinglogfiles
 
+import (
+	"fmt"
+	"regexp"
+)
+
 func IsValidLine(text string) bool {
-	panic("Please implement the IsValidLine function")
+	re := regexp.MustCompile(`^\[(TRC|DBG|INF|WRN|ERR|FTL)\]`)
+	return re.MatchString(text)
 }
 
 func SplitLogLine(text string) []string {
-	panic("Please implement the SplitLogLine function")
+	re := regexp.MustCompile(`<[~*=-]*>`)
+	return re.Split(text, -1)
 }
 
 func CountQuotedPasswords(lines []string) int {
-	panic("Please implement the CountQuotedPasswords function")
+	re := regexp.MustCompile(`(?i)"[^"]*password[^"]*"`)
+	sum := 0
+
+	for _, line := range lines {
+		if re.MatchString(line) {
+			sum += 1
+		}
+	}
+
+	return sum
 }
 
 func RemoveEndOfLineText(text string) string {
-	panic("Please implement the RemoveEndOfLineText function")
+	re := regexp.MustCompile(`end-of-line\d+`)
+	return re.ReplaceAllString(text, "")
 }
 
 func TagWithUserName(lines []string) []string {
-	panic("Please implement the TagWithUserName function")
+	res := []string{}
+	re := regexp.MustCompile(`User\s+(\w+\d+)`)
+
+	for _, line := range lines {
+		if re.MatchString(line) {
+			sub := re.FindStringSubmatch(line)
+			line = fmt.Sprintf("[USR] %s %s", sub[1], line)
+		}
+		res = append(res, line)
+	}
+
+	return res
 }
