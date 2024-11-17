4ffb745136f6a062bda3ae91320121583ae3aaff
Exiles.prx <exiles.prx@gmail.com>
2024-11-17 17:38:51 -0500

    Solved exercise two fer

--- two-fer/two_fer.go
+++ two-fer/two_fer.go
@@ -5,11 +5,16 @@
 // https://golang.org/doc/effective_go.html#commentary
 package twofer
 
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
 // ShareWith should have a comment documenting it.
 func ShareWith(name string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+	if utf8.RuneCountInString(name) == 0 {
+		return "One for you, one for me."
+	}
+
+	return fmt.Sprintf("One for %s, one for me.", name)
 }
