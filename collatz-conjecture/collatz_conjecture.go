fbfe494d5f70fabf8ff48496a58b5d5e280e691c
Exiles.prx <exiles.prx@gmail.com>
2024-11-17 18:04:13 -0500

    Solved exercise collatz conjecture

--- collatz-conjecture/collatz_conjecture.go
+++ collatz-conjecture/collatz_conjecture.go
@@ -1,5 +1,22 @@
 package collatzconjecture
 
+import "fmt"
+
 func CollatzConjecture(n int) (int, error) {
-	panic("Please implement the CollatzConjecture function")
+	if n <= 0 {
+		return 0, fmt.Errorf("Cannot be less than or equal to 0")
+	}
+
+	steps := 0
+	for n != 1 {
+		if n%2 == 0 {
+			n = n / 2
+		} else {
+			n = 3*n + 1
+		}
+
+		steps++
+	}
+
+	return steps, nil
 }
