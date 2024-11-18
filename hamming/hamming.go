d57df5fc99aa846772fbf8e02f5a6ea8df67ad90
Exiles.prx <exiles.prx@gmail.com>
2024-11-17 19:25:02 -0500

    Solved exercise hamming

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,5 +1,20 @@
 package hamming
 
+import (
+	"errors"
+	"unicode/utf8"
+)
+
 func Distance(a, b string) (int, error) {
-	panic("Implement the Distance function")
+	if utf8.RuneCountInString(a) != utf8.RuneCountInString(b) {
+		return 0, errors.New("String must be of equal length")
+	}
+
+	distance := 0
+	for i, r := range a {
+		if r != rune(b[i]) {
+			distance += 1
+		}
+	}
+	return distance, nil
 }
