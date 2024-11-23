73447a36b49e0ae69d7bf65395e64ba5e34afa71
Exiles.prx <exiles.prx@gmail.com>
2024-11-23 10:56:28 -0500

    Solved exercise rna transcription

--- rna-transcription/rna_transcription.go
+++ rna-transcription/rna_transcription.go
@@ -1,5 +1,19 @@
 package strand
 
 func ToRNA(dna string) string {
-	panic("Please implement the ToRNA function")
+	rna := ""
+	for _, c := range dna {
+		switch c {
+		case 'G':
+			rna += "C"
+		case 'C':
+			rna += "G"
+		case 'T':
+			rna += "A"
+		case 'A':
+			rna += "U"
+		}
+	}
+
+	return rna
 }
