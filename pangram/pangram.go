44ad66a1b9fbaa3ed0f9ea4ab180d632ffc9bb31
Exiles.prx <exiles.prx@gmail.com>
2024-11-24 12:48:30 -0500

    Solved exercise pangram

--- pangram/pangram.go
+++ pangram/pangram.go
@@ -1,5 +1,17 @@
 package pangram
 
+import (
+	"strings"
+	"unicode"
+)
+
 func IsPangram(input string) bool {
-	panic("Please implement the IsPangram function")
+	alphabet := map[rune]bool{}
+	for _, r := range strings.ToUpper(input) {
+		if unicode.IsLetter(r) {
+			alphabet[r] = true
+		}
+	}
+
+	return len(alphabet) == 26
 }
