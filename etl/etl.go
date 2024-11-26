c120d19d6bf166c86ed4b1e2ef62eb872fed06f3
Exiles.prx <exiles.prx@gmail.com>
2024-11-25 21:01:48 -0500

    Solved exercise etl

--- etl/etl.go
+++ etl/etl.go
@@ -1,5 +1,14 @@
 package etl
 
+import "strings"
+
 func Transform(in map[int][]string) map[string]int {
-	panic("Please implement the Transform function")
+	mapping := map[string]int{}
+	for points, letters := range in {
+		for _, letter := range letters {
+			mapping[strings.ToLower(letter)] = points
+		}
+	}
+
+	return mapping
 }
