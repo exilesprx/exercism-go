5bab2d0af34564c568cef8e7243bd1b0b83f93b5
Exiles.prx <exiles.prx@gmail.com>
2024-11-26 22:11:26 -0500

    Solved exercise grains

--- grains/grains.go
+++ grains/grains.go
@@ -1,9 +1,17 @@
 package grains
 
+import (
+	"errors"
+)
+
 func Square(number int) (uint64, error) {
-	panic("Please implement the Square function")
+	if number <= 0 || number > 64 {
+		return 0, errors.New("Must be greater than 0")
+	}
+
+	return 1 << (number - 1), nil
 }
 
 func Total() uint64 {
-	panic("Please implement the Total function")
+	return 1<<64 - 1
 }
