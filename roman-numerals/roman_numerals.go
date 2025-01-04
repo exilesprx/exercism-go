712fb0e3415035261cc9deef115f9af78c446840
Exiles.prx <exiles.prx@gmail.com>
2025-01-04 15:20:16 -0500

    Solved roman numerals

--- roman-numerals/roman_numerals.go
+++ roman-numerals/roman_numerals.go
@@ -1,5 +1,45 @@
 package romannumerals
 
+import (
+	"errors"
+)
+
+type RomanNumeral struct {
+	letters string
+	value   int
+}
+
+var ruleSet = []RomanNumeral{
+	{"M", 1000},
+	{"CM", 900},
+	{"D", 500},
+	{"CD", 400},
+	{"C", 100},
+	{"XC", 90},
+	{"L", 50},
+	{"XL", 40},
+	{"X", 10},
+	{"IX", 9},
+	{"V", 5},
+	{"IV", 4},
+	{"I", 1},
+}
+
 func ToRomanNumeral(input int) (string, error) {
-	panic("Please implement the ToRomanNumeral function")
+	if input <= 0 || input >= 4000 {
+		return "", errors.New("invalid number")
+	}
+
+	romanNumerals := ""
+	for input > 0 {
+		for _, r := range ruleSet {
+			if input >= r.value {
+				input = input - r.value
+				romanNumerals += r.letters
+				break
+			}
+		}
+	}
+
+	return romanNumerals, nil
 }
