0a1f8e17b3ea701b49337e7b445883c7e26c9743
Exiles.prx <exiles.prx@gmail.com>
2025-01-07 18:07:41 -0500

    Solved exercise isbn verifier

--- isbn-verifier/isbn_verifier.go
+++ isbn-verifier/isbn_verifier.go
@@ -1,5 +1,30 @@
 package isbn
 
+import (
+	"strings"
+	"unicode"
+	"unicode/utf8"
+)
+
 func IsValidISBN(isbn string) bool {
-	panic("Please implement the IsValidISBN function")
+	isbn = strings.ReplaceAll(isbn, "-", "")
+	if chars := utf8.RuneCountInString(isbn); chars != 10 {
+		return false
+	}
+
+	sum := 0
+	for i, r := range isbn {
+		if i == 9 && r == 'X' {
+			sum += 10
+			continue
+		}
+
+		if unicode.IsLetter(r) {
+			return false
+		}
+
+		sum += int(r-'0') * (10 - i)
+	}
+
+	return sum%11 == 0
 }
