dafac6fed8fa0aed31daa5a5203d4f96af1c047e
Exiles.prx <exiles.prx@gmail.com>
2025-01-19 19:57:45 -0500

    Solved exercise triangle

--- triangle/triangle.go
+++ triangle/triangle.go
@@ -1,28 +1,29 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
 // Package triangle should have a package comment that summarizes what it's about.
 // https://golang.org/doc/effective_go.html#commentary
 package triangle
 
+// The different kinds of triangles
+type Kind int
 
-// Notice KindFromSides() returns this type. Pick a suitable data type.
-type Kind
-
+// All the different kinds of triangles represented by the Kind type, which is an int
 const (
-    // Pick values for the following identifiers used by the test program.
-    NaT // not a triangle
-    Equ // equilateral
-    Iso // isosceles
-    Sca // scalene
+	NaT = iota
+	Equ
+	Iso
+	Sca
 )
 
-// KindFromSides should have a comment documenting it.
+// KindFromSides returns what kind of triangle is formed by the three sides
 func KindFromSides(a, b, c float64) Kind {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	var k Kind
-	return k
+	if a <= 0 || b <= 0 || c <= 0 || a+b < c || b+c < a || a+c < b {
+		return NaT
+	}
+
+	if a == b && b == c {
+		return Equ
+	} else if a == b || b == c || c == a {
+		return Iso
+	}
+
+	return Sca
 }
