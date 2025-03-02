d9ee65910bff6e9c9b56f1d6800e00fcc90641b3
Exiles.prx <exiles.prx@gmail.com>
2025-03-02 15:48:02 -0500

    Solved exercise anagram

--- anagram/anagram.go
+++ anagram/anagram.go
@@ -1,5 +1,37 @@
 package anagram
 
+import (
+	"strings"
+	"unicode"
+	"unicode/utf8"
+)
+
 func Detect(subject string, candidates []string) []string {
-	panic("Please implement the Detect function")
+	anagrams := []string{}
+	for _, candidate := range candidates {
+		if strings.EqualFold(candidate, subject) || utf8.RuneCountInString(candidate) != utf8.RuneCountInString(subject) {
+			continue
+		}
+
+		letters := map[rune]int{}
+		matches := map[rune]int{}
+		for _, letter := range subject {
+			letters[unicode.ToLower(letter)]++
+		}
+
+		isAnagram := true
+		for _, letter := range candidate {
+			if value, ok := letters[unicode.ToLower(letter)]; !ok || value == matches[unicode.ToLower(letter)] {
+				isAnagram = false
+				break
+			}
+
+			matches[unicode.ToLower(letter)]++
+		}
+
+		if isAnagram {
+			anagrams = append(anagrams, candidate)
+		}
+	}
+	return anagrams
 }
