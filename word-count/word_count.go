a5da24ad8171f418ebca5b2e66c7a4c2ad0f221c
Exiles.prx <exiles.prx@gmail.com>
2025-03-02 18:05:30 -0500

    Solved exercise word count

--- word-count/word_count.go
+++ word-count/word_count.go
@@ -1,7 +1,25 @@
 package wordcount
 
+import (
+	"strings"
+	"unicode"
+)
+
 type Frequency map[string]int
 
 func WordCount(phrase string) Frequency {
-	panic("Please implement the WordCount function")
+	f := func(c rune) bool {
+		return c != '\'' && (unicode.IsSpace(c) || unicode.IsPunct(c) || unicode.IsSymbol(c))
+	}
+
+	frequency := Frequency{}
+	words := strings.FieldsFunc(phrase, f)
+	for _, word := range words {
+		word = strings.Trim(word, "'\"")
+		if word == "" {
+			continue
+		}
+		frequency[strings.ToLower(word)]++
+	}
+	return frequency
 }
