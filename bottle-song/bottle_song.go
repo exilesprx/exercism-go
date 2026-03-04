afbd9afc5a9cc87d10e9d82c0b09f7eb201fce9e
Exiles.prx <exiles.prx@gmail.com>
2026-03-04 16:18:22 -0500

    fix: clean up

--- bottle-song/bottle_song.go
+++ bottle-song/bottle_song.go
@@ -8,21 +8,10 @@ import (
 
 // Recite returns the verses of the "99 Bottles of Beer" song, starting from startBottles and taking down takeDown bottles.
 func Recite(startBottles, takeDown int) []string {
-	phrases := []string{
-		"%s green %s hanging on the wall,",
-		"%s green %s hanging on the wall,",
-		"And if one green bottle should accidentally fall,",
-		"There'll be %s green %s hanging on the wall.",
-	}
 	result := []string{}
 	for i := range takeDown {
 		num := startBottles - i
-		result = append(result,
-			fmt.Sprintf(phrases[0], numToWord(num), numToBottle(num)),
-			fmt.Sprintf(phrases[1], numToWord(num), numToBottle(num)),
-			phrases[2],
-			fmt.Sprintf(phrases[3], strings.ToLower(numToWord(num-1)), numToBottle(num-1)),
-		)
+		result = append(result, verse(num)...)
 		if i != takeDown-1 {
 			result = append(result, "")
 		}
@@ -30,38 +19,23 @@ func Recite(startBottles, takeDown int) []string {
 	return result
 }
 
-func numToWord(num int) string {
-	switch num {
-	case 0:
-		return "No"
-	case 1:
-		return "One"
-	case 2:
-		return "Two"
-	case 3:
-		return "Three"
-	case 4:
-		return "Four"
-	case 5:
-		return "Five"
-	case 6:
-		return "Six"
-	case 7:
-		return "Seven"
-	case 8:
-		return "Eight"
-	case 9:
-		return "Nine"
-	case 10:
-		return "Ten"
-	default:
-		return ""
+func verse(num int) []string {
+	return []string{
+		fmt.Sprintf("%s green bottle%s hanging on the wall,", numToWord(num), plural(num)),
+		fmt.Sprintf("%s green bottle%s hanging on the wall,", numToWord(num), plural(num)),
+		"And if one green bottle should accidentally fall,",
+		fmt.Sprintf("There'll be %s green bottle%s hanging on the wall.", strings.ToLower(numToWord(num-1)), plural(num-1)),
 	}
 }
 
-func numToBottle(num int) string {
-	if num == 1 {
-		return "bottle"
+func numToWord(num int) string {
+	words := []string{"No", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"}
+	return words[num]
+}
+
+func plural(num int) string {
+	if num != 1 {
+		return "s"
 	}
-	return "bottles"
+	return ""
 }
