4450b403a1ba950ca33290b52c7001f7505ab2fc
Exiles.prx <exiles.prx@gmail.com>
2026-03-06 12:02:25 -0500

    fix: solved exercise line up

--- line-up/line_up.go
+++ line-up/line_up.go
@@ -1,7 +1,28 @@
+// Package lineup provides a function to format a message for customers in a line.
 package lineup
 
-import "fmt"
+import (
+	"fmt"
+)
 
+// Format takes a customer's name and their position in line, and returns a formatted message.
 func Format(name string, number int) string {
-	panic("Please implement the Format function")
+	return fmt.Sprintf("%s, you are the %d%s customer we serve today. Thank you!", name, number, suffix(number))
+}
+
+// suffix returns the appropriate ordinal suffix for a given number.
+func suffix(number int) string {
+	if number%100 >= 11 && number%100 <= 13 {
+		return "th"
+	}
+	if number%10 == 1 {
+		return "st"
+	}
+	if number%10 == 2 {
+		return "nd"
+	}
+	if number%10 == 3 {
+		return "rd"
+	}
+	return "th"
 }
