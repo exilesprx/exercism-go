0722beab8e5f5767e6e742ca276bf11027ac0081
Exiles.prx <exiles.prx@gmail.com>
2026-03-06 17:29:33 -0500

    fix: solved exercise list ops

--- list-ops/list_ops.go
+++ list-ops/list_ops.go
@@ -1,36 +1,78 @@
+// Package listops implements a number of list operations on lists of integers without using built-in functions
 package listops
 
 // IntList is an abstraction of a list of integers which we can define methods on
 type IntList []int
 
 func (s IntList) Foldl(fn func(int, int) int, initial int) int {
-	panic("Please implement the Foldl function")
+	acc := initial
+	for _, v := range s {
+		acc = fn(acc, v)
+	}
+	return acc
 }
 
 func (s IntList) Foldr(fn func(int, int) int, initial int) int {
-	panic("Please implement the Foldr function")
+	acc := initial
+	for i := s.Length() - 1; i >= 0; i-- {
+		acc = fn(s[i], acc)
+	}
+	return acc
 }
 
 func (s IntList) Filter(fn func(int) bool) IntList {
-	panic("Please implement the Filter function")
+	lst := make(IntList, 0)
+	for _, v := range s {
+		if fn(v) {
+			lst = lst.Append(IntList{v})
+		}
+	}
+	return lst
 }
 
 func (s IntList) Length() int {
-	panic("Please implement the Length function")
+	i := 0
+	for range s {
+		i++
+	}
+	return i
 }
 
 func (s IntList) Map(fn func(int) int) IntList {
-	panic("Please implement the Map function")
+	lst := make(IntList, s.Length())
+	for i, v := range s {
+		lst[i] = fn(v)
+	}
+	return lst
 }
 
 func (s IntList) Reverse() IntList {
-	panic("Please implement the Reverse function")
+	lstLen := s.Length()
+	lst := make(IntList, lstLen)
+	for i, v := range s {
+		lst[lstLen-1-i] = v
+	}
+	return lst
 }
 
 func (s IntList) Append(lst IntList) IntList {
-	panic("Please implement the Append function")
+	l := make(IntList, s.Length()+lst.Length())
+	i := 0
+	for _, v := range s {
+		l[i] = v
+		i++
+	}
+	for _, v := range lst {
+		l[i] = v
+		i++
+	}
+	return l
 }
 
 func (s IntList) Concat(lists []IntList) IntList {
-	panic("Please implement the Concat function")
+	lst := s.Append(IntList{})
+	for _, v := range lists {
+		lst = lst.Append(v)
+	}
+	return lst
 }
