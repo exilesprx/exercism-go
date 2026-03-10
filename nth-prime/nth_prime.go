53c75fb2230f0f40160a457fad6300c248d599b7
Exiles.prx <exiles.prx@gmail.com>
2026-03-10 19:36:00 -0400

    fix: math.Sqrt optimize

--- nth-prime/nth_prime.go
+++ nth-prime/nth_prime.go
@@ -3,7 +3,6 @@ package nthprime
 
 import (
 	"fmt"
-	"math"
 )
 
 // Nth returns the nth prime number. An error must be returned if the nth prime number can't be calculated ('n' is equal or less than zero)
@@ -31,7 +30,7 @@ func isPrime(n int) bool {
 		return false
 	}
 
-	for i := 5; i <= int(math.Sqrt(float64(n))); i += 6 {
+	for i := 5; i*i <= n; i += 6 {
 		if n%i == 0 || n%(i+2) == 0 {
 			return false
 		}
